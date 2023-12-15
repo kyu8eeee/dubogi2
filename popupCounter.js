cf7a0689c3ffbf2a2e3f7afe2916d149718753a9
kyu8eeee <130030554+kyu8eeee@users.noreply.github.com>
2023-12-15 22:53:41 +0900

    Update popupCounter.js

--- popupCounter.js
+++ popupCounter.js
@@ -1,30 +1,35 @@
-var redirect = function() {
-    window.location.href = 'https://link.coupang.com/a/bjybMs';
-    setVisitedCookie();
-}
-
-var removePopupAndOverlay = function() {
-    document.getElementById('myPopup').style.display = 'none';
-    document.getElementById('overlay').style.display = 'none';
-    document.body.style.overflow = 'auto'; // Enable scroll
-    setVisitedCookie();
-}
-
-var setVisitedCookie = function() {
-    document.cookie = "visited=true; max-age=7200"; // 2 hours
-}
-
 window.onload = function() {
+    var redirect = function() {
+        window.location.href = 'https://link.coupang.com/a/bjybMs';
+        setVisitedCookie();
+    }
+
+    var removePopupAndOverlay = function() {
+        document.getElementById('myPopup').style.display = 'none';
+        document.getElementById('overlay').style.display = 'none';
+        document.body.style.overflow = 'auto'; // Enable scroll
+        setVisitedCookie();
+    }
+
+    var setVisitedCookie = function() {
+        document.cookie = "visited=true; max-age=7200"; // 2 hours
+    }
+
     if (document.cookie.includes("visited=true")) {
         removePopupAndOverlay();
     }
 
     let counter = 5;
     const counterElement = document.getElementById('counter');
+    const closeBtnContainer = document.getElementById('close-btn-container');
+    const visitElement = document.getElementById('visit');
+
+    closeBtnContainer.addEventListener('click', removePopupAndOverlay);
+    visitElement.addEventListener('click', redirect);
 
     setInterval(function() {
         if (counter > 0) {
-            counterElement.innerText = counter--;
+            counterElement.innerText = --counter;
         } else {
             counterElement.innerText = 'x';
         }
